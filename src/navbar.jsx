bb7b0bb47f5772862f60883cc55709dbe0e8dfb0
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2021-03-22 02:44:36 +0530

    added mobile navigation menu

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,24 +1,26 @@
 import { useState, useEffect, createRef } from "react";
 import styles from "../styles/navbar.module.scss";
 import MenuTwoToneIcon from "@material-ui/icons/MenuTwoTone";
+import CloseIcon from "@material-ui/icons/Close";
 
 export function Navbar() {
     const navbarLinks = [
         { name: "aaa", link: "#" },
-        { name: "aaa", link: "#" },
-        { name: "aaa", link: "#" },
+        { name: "bbb", link: "#" },
+        { name: "ccc", link: "#" },
     ];
 
-    const [scroll, setScroll] = useState(false);
+    const [isScrolled, setScrolled] = useState(false);
+    const [isMenuClicked, setMenuClicked] = useState(false);
 
     useEffect(() => {
         const listener = document.addEventListener("scroll", (e) => {
             const scrolled = document.scrollingElement.scrollTop;
 
             if (scrolled > 300) {
-                setScroll(true);
+                setScrolled(true);
             } else {
-                setScroll(false);
+                setScrolled(false);
             }
         });
 
@@ -27,20 +29,39 @@ export function Navbar() {
         };
     });
 
+    const handleHamburgerMenu = () => {
+        setMenuClicked(true);
+    };
+    const handleCloseButton = () => {
+        setMenuClicked(false);
+    };
+
     return (
         <nav
             className={
-                scroll
+                isScrolled
                     ? `${styles.navbar} ${styles.changeBackgroundColor}`
                     : `${styles.navbar}`
             }
         >
             <div>Lorem, ipsum.</div>
 
-            <MenuTwoToneIcon fontSize="large" />
-            {/* {navbarLinks.map((link) => (
-                <li>{link.name}</li>
-            ))} */}
+            <MenuTwoToneIcon fontSize="large" onClick={handleHamburgerMenu} />
+            <ul
+                className={
+                    isMenuClicked ? `${styles.mobileMenu}` : `${styles.hide}`
+                }
+            >
+                <CloseIcon
+                    color="inherit"
+                    className={styles.close}
+                    fontSize="inherit"
+                    onClick={handleCloseButton}
+                />
+                {navbarLinks.map((link) => (
+                    <li key={link.name}>{link.name}</li>
+                ))}
+            </ul>
         </nav>
     );
 }
