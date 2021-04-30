81700087c3d7db7566cd7993270804589d3c041f
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2021-04-30 16:29:56 +0530

    isolated navbar from header

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
                 <title>Create Next App</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <Navbar className="navbarHeaderAbsolute" />
 
             <Header />
             <Body />
--- src/header.jsx
+++ src/header.jsx
@@ -4,7 +4,6 @@ import { Navbar } from "../src/navbar";
 export function Header() {
     return (
         <header className={styles.initialPage}>
-            <Navbar className="navbarHeaderAbsolute" />
             <h3>Advisory Services for Growing Companies</h3>
         </header>
     );
