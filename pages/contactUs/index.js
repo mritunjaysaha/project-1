205e42a15a1382868dfad7e648e8c4faa8cf74ec
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2021-05-06 16:32:40 +0530

    completed contact us section

--- pages/contactUs/index.js
+++ pages/contactUs/index.js
@@ -1,3 +1,15 @@
+import styles from "../../styles/contactUs.module.scss";
+import { Anchor } from "../../src/atoms/anchor";
+
 export default function ContactUsPage() {
-    return <h1>contact us</h1>;
+    return (
+        <section className={styles.contactUsSection}>
+            <div className={styles.contentsContainer}>
+                <h1>Get in touch with us</h1>
+                <div className={styles.anchorContainer}>
+                    <Anchor name="contact us" href="/" />
+                </div>
+            </div>
+        </section>
+    );
 }
