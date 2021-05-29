23cada8cd177e9bbbe55e6b56e486bf615f2fcfb
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2021-05-29 22:27:13 +0530

    made changes to blogData.js

--- pages/blog/[name].jsx
+++ pages/blog/[name].jsx
@@ -6,15 +6,13 @@ import styles from "../../styles/blogPages.module.scss";
 export default function BlogPage() {
     const router = useRouter();
     const { name } = router.query;
-    console.log("name: ", name);
 
     const [currentData, setCurrentData] = useState("");
 
     const { data } = blogData;
-
     useEffect(() => {
         for (let datum of data) {
-            if (name === datum.name) {
+            if (name === datum.title) {
                 setCurrentData(datum);
                 break;
             }
@@ -23,24 +21,23 @@ export default function BlogPage() {
 
     return (
         <section className={styles.blogSection}>
+            {name}
             <header className={styles.header}>
                 <img src={currentData.img} alt={name} />
-                <h1>{name ? name.replaceAll("-", " ") : "name"}</h1>
+                <h1>{name}</h1>
             </header>
             <section className={styles.blogContentsContainer}>
-                {currentData
+                {/* {currentData
                     ? currentData.fullDescription.map((data) => {
                           const { head, desc } = data;
                           return (
                               <div className={styles.blogContainer}>
                                   <h3>{head}</h3>
-                                  {desc.map((d) => {
-                                      return <p>{d}</p>;
-                                  })}
                               </div>
                           );
                       })
-                    : ""}
+                    : ""
+                    } */}
             </section>
         </section>
     );
