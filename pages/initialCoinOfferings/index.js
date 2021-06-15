c1f9f4c12b54ead1b732539288d338c105553105
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2021-06-15 20:10:09 +0530

    added company name

--- pages/initialCoinOfferings/index.js
+++ pages/initialCoinOfferings/index.js
@@ -1,5 +1,7 @@
 import { ServicePage } from "../../src/servicePages";
 
 export default function InitialCoinOfferings() {
-    return <ServicePage name={name} data={data} imgSrc={img} />;
+    // return <ServicePage name={name} data={data} imgSrc={img} />;
+
+    return <></>;
 }
