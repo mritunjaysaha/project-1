f914d8e79f15582d77db1916f31545cc3960f754
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-06-18 10:19:32 +0530

    added parallax effect with particles js

--- pages/blog/[name].jsx
+++ pages/blog/[name].jsx
@@ -3,6 +3,129 @@ import { useRouter } from "next/router";
 import { blogData } from "../../blogsData";
 import Particles from "react-tsparticles";
 import styles from "../../styles/blogPages.module.scss";
+import Parallax from "parallax-js";
+
+const ParticlesJS = () => (
+    <Particles
+        id="particle-js"
+        className={styles.particleJS}
+        options={{
+            background: {
+                color: {
+                    value: "#0d47a1",
+                },
+            },
+            fpsLimit: 60,
+            particles: {
+                number: {
+                    value: 120,
+                    density: {
+                        enable: true,
+                        value_area: 800,
+                    },
+                },
+                color: {
+                    value: "#ffffff",
+                },
+                shape: {
+                    type: "circle",
+                    stroke: {
+                        width: 0,
+                        color: "#000000",
+                    },
+                    polygon: {
+                        nb_sides: 5,
+                    },
+                    image: {
+                        src: "img/github.svg",
+                        width: 100,
+                        height: 100,
+                    },
+                },
+                opacity: {
+                    value: 0.5,
+                    random: false,
+                    anim: {
+                        enable: false,
+                        speed: 1,
+                        opacity_min: 0.1,
+                        sync: false,
+                    },
+                },
+                size: {
+                    value: 3,
+                    random: true,
+                    anim: {
+                        enable: false,
+                        speed: 1,
+                        size_min: 0.1,
+                        sync: false,
+                    },
+                },
+                line_linked: {
+                    enable: true,
+                    distance: 150,
+                    color: "#ffffff",
+                    opacity: 0.4,
+                    width: 1,
+                },
+                move: {
+                    enable: true,
+                    speed: 1,
+                    direction: "none",
+                    random: false,
+                    straight: false,
+                    out_mode: "out",
+                    bounce: false,
+                    attract: {
+                        enable: false,
+                        rotateX: 600,
+                        rotateY: 1200,
+                    },
+                },
+            },
+            interactivity: {
+                detect_on: "canvas",
+                events: {
+                    onhover: {
+                        enable: true,
+                        mode: "grab",
+                    },
+                    onclick: {
+                        enable: true,
+                        mode: "push",
+                    },
+                    resize: true,
+                },
+                modes: {
+                    grab: {
+                        distance: 140,
+                        line_linked: {
+                            opacity: 1,
+                        },
+                    },
+                    bubble: {
+                        distance: 400,
+                        size: 40,
+                        duration: 2,
+                        opacity: 8,
+                        speed: 3,
+                    },
+                    repulse: {
+                        distance: 200,
+                        duration: 0.4,
+                    },
+                    push: {
+                        particles_nb: 4,
+                    },
+                    remove: {
+                        particles_nb: 2,
+                    },
+                },
+            },
+        }}
+    />
+);
 
 export default function BlogPage() {
     const router = useRouter();
@@ -11,151 +134,36 @@ export default function BlogPage() {
     const [currentData, setCurrentData] = useState("");
 
     const { data } = blogData;
+
     useEffect(() => {
-        for (let datum of data) {
-            if (name === datum.title) {
-                setCurrentData(datum);
-                break;
-            }
-        }
-    }, [currentData]);
+        // for (let datum of data) {
+        //     if (name === datum.title) {
+        //         setCurrentData(datum);
+        //         break;
+        //     }
+        // }
+    });
 
-    if (process.browser) {
-        require("pathseg");
-    }
-
-    function handleParticles(event) {
-        const target = document.getElementById("particles-js");
-        target.x = event.offsetX;
-        target.y = event.offsetY;
-    }
+    useEffect(() => {
+        const scene = document.querySelector("#scene");
+        new Parallax(scene);
+    });
 
     return (
         <section className={styles.blogSection}>
-            <header className={styles.header}>
-                {/* <img src={currentData.img} alt={name} />
-                <h1>{name}</h1> */}
-                {/* <Particles
-                    id="particles-js"
-                    onMouseMove={handleParticles}
-                    options={{
-                        background: {
-                            color: {
-                                value: "#0d47a1",
-                            },
-                        },
-                        fpsLimit: 60,
-                        particles: {
-                            number: {
-                                value: 80,
-                                density: {
-                                    enable: true,
-                                    value_area: 800,
-                                },
-                            },
-                            color: {
-                                value: "#ffffff",
-                            },
-                            shape: {
-                                type: "circle",
-                                stroke: {
-                                    width: 0,
-                                    color: "#000000",
-                                },
-                                polygon: {
-                                    nb_sides: 5,
-                                },
-                                image: {
-                                    src: "img/github.svg",
-                                    width: 100,
-                                    height: 100,
-                                },
-                            },
-                            opacity: {
-                                value: 0.5,
-                                random: false,
-                                anim: {
-                                    enable: false,
-                                    speed: 1,
-                                    opacity_min: 0.1,
-                                    sync: false,
-                                },
-                            },
-                            size: {
-                                value: 5,
-                                random: true,
-                                anim: {
-                                    enable: false,
-                                    speed: 20,
-                                    size_min: 0.1,
-                                    sync: false,
-                                },
-                            },
-                            line_linked: {
-                                enable: true,
-                                distance: 150,
-                                color: "#ffffff",
-                                opacity: 0.4,
-                                width: 1,
-                            },
-                            move: {
-                                enable: true,
-                                speed: 1,
-                                direction: "none",
-                                random: true,
-                                straight: false,
-                                out_mode: "bounce",
-                                bounce: true,
-                                line_linked: true,
-                                attract: {
-                                    enable: false,
-                                    rotateX: 600,
-                                    rotateY: 1200,
-                                },
-                            },
-                        },
-                        interactivity: {
-                            detect_on: "window",
-                            events: {
-                                onhover: {
-                                    enable: true,
-                                    mode: "repulse",
-                                },
-                                resize: true,
-                            },
-                            modes: {
-                                // grab: {
-                                //     distance: 400,
-                                //     line_linked: {
-                                //         opacity: 1,
-                                //     },
-                                // },
-                                bubble: {
-                                    distance: 400,
-                                    size: 40,
-                                    duration: 2,
-                                    opacity: 8,
-                                    speed: 3,
-                                },
-                                repulse: {
-                                    distance: 200,
-                                    duration: 0.4,
-                                },
-                                // push: {
-                                //     particles_nb: 4,
-                                // },
-                                // remove: {
-                                //     particles_nb: 2,
-                                // },
-                            },
-                        },
-
-                        detectRetina: true,
-                    }}
-                /> */}
+            <header className={styles.parallax} id="scene">
+                {/* <div data-depth="0.6" className={styles.layer}>
+                    <div className={styles.someSpace}>
+                        <h1>TEMP </h1>
+                    </div>
+                </div> */}
+                <div data-depth="0.4" className={styles.layer}>
+                    <ParticlesJS />
+                </div>
+                <div data-depth="0.3" className={styles.layer}></div>
             </header>
-            <section className={styles.blogContentsContainer}>
-                {/* {currentData
+            {/* <section className={styles.blogContentsContainer}>
+                {currentData
                     ? currentData.fullDescription.map((data) => {
                           const { head, desc } = data;
                           return (
@@ -165,8 +173,8 @@ export default function BlogPage() {
                           );
                       })
                     : ""
-                    } */}
-            </section>
+                    }
+            </section> */}
         </section>
     );
 }
