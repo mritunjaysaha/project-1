c6080e2482f26a6f9ea9b3a2b08322beff4d2648
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-06-24 13:51:50 +0530

    page will open in current page

--- src/atoms/anchor.jsx
+++ src/atoms/anchor.jsx
@@ -4,7 +4,7 @@ import styles from "../../styles/anchor.module.scss";
 export function Anchor({ href, name }) {
     return (
         <Link href={href ? href : "#"}>
-            <a rel="noopener" target="_blank" className={styles.anchor}>
+            <a rel="noopener" className={styles.anchor}>
                 {name}
             </a>
         </Link>
