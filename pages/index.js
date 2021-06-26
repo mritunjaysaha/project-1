f875bdba0eeafb5e81f9232a344a35a4e87481cb
Mritunjay Saha <mritunjaysaha17@gmail.com>
2021-06-26 20:51:35 +0530

    added mailchimp script

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,17 @@ export default function Home() {
                 <title>{companyName ? companyName.name : ""}</title>
                 <link rel="icon" href="/favicon.ico" />
                 <meta name="description" content="Maneuver Ventures" />
+                <script id="mcjs">
+                    !function(c,h,i,m,p)
+                    {
+                        ((m = c.createElement(h)),
+                        (p = c.getElementsByTagName(h)[0]),
+                        (m.async = 1),
+                        (m.src = i),
+                        p.parentNode.insertBefore(m, p))
+                    }
+                    (document,"script","https://chimpstatic.com/mcjs-connected/js/users/e654510bb63cf53256868ec7e/8116d5a2085af6e4113b8d126.js");
+                </script>
             </Head>
 
             <Header />
